5c8cac2bc4da572abdd6b3b7c1327bd1b86395d9
New Dad <josh_panakkal3122@hotmail.com>
2020-08-21 23:21:52 -0400

    create route for selectAll burgers / console.log data

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,16 @@
 const express = require("express");
-const burger = require("../models/burger");
+const burgers = require("../models/burger");
+var router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burgers.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+    //   res.render("index", hbsObject);
+    });
+});
+  
+  module.exports = router;
