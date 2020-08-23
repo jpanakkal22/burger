772d7402f512fe87ffde99ac49e8cf55a7a18816
New Dad <josh_panakkal3122@hotmail.com>
2020-08-22 23:07:44 -0400

    update expressions in handlebars files

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,4 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+ // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".change-burger").on("click", function(event) {
     var id = $(this).data("id");
