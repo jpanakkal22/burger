5bbd199630c60f2c36e50123aec3140c45214704
New Dad <josh_panakkal3122@hotmail.com>
2020-08-23 17:32:26 -0400

    add function objToSql and modify updateOne method

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,29 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM ??";
@@ -37,13 +60,23 @@ var orm = {
       cb(result);
     });
   },
-  updateOne: function(table, cols, value, cb) {
-    var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-    connection.query(queryString, [table, col1, val2, col2, val2], function(err, result) {
-      if (err) throw err;
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
       cb(result);
     });
-  },
+  }
   
 };
 
