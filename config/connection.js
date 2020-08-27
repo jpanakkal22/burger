fc10836fb9b837d459e0e932a08cb7aae2c5e231
New Dad <josh_panakkal3122@hotmail.com>
2020-08-26 21:59:14 -0400

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,6 @@
 var mysql = require("mysql");
 
 var connection;
-//  = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "Contigo%5",
-//   database: "burgers_db"
-// });
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
